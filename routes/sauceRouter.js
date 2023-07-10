bd7c93cfcab2c5f62e6972b6e98bee0c2653a191
Oscar Calle <ocalle374@gmail.com>
2023-07-10 14:44:59 -0400

    sauceRouter needs a put m ethod, did the other to test on postman

--- routes/sauceRouter.js
+++ routes/sauceRouter.js
@@ -6,6 +6,7 @@ const Sauce = require('../models/sauce.js') // allows me to use the Sauce model 
 
 
 
+
 //GET all
 sauceRouter.get("/", (request, response, next) => {
     Sauce.find()
@@ -19,6 +20,22 @@ sauceRouter.get("/", (request, response, next) => {
         })
 })
 
+//GET One
+sauceRouter.get("/:sauceId", (request, response, next) => {
+    const sauceId = request.params.sauceId;
+    Sauce.findById(sauceId)
+        .then(foundSauce => {
+            if (!foundSauce) {
+                return response.status(404).send(foundSauce)
+            }
+            response.status(200).send(foundSauce)
+        })
+        .catch(error => {
+            response.status(500)
+            return next(error)
+        })
+});
+
 // POST(add one); remember this goes throught the sauces.js and server.js(middleware) so have the correct name in postman --- http://localhost:8100/sauces/
 sauceRouter.post("/", (request, response, next) => {
     const newSauce = new Sauce(request.body)
@@ -31,6 +48,22 @@ sauceRouter.post("/", (request, response, next) => {
             response.status(500)
             return next(error)
         })
+});
+
+//DELETE one
+sauceRouter.delete("/:sauceId", (request, response, next) => {
+    Sauce.findOneAndDelete({_id: request.params.sauceId})
+    .exec()
+    .then(deletedSauce => {
+        if(!deletedSauce) {
+            return response.status(404).send("Sauce not found")
+        }
+        return response.status(200).send("Sauce found")
+    })
+    .catch(error => {
+        response.status(500)
+        return next(error)
+    })
 })
 
 
