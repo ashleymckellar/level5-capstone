50ea7442b0610d68a4f62bd4d2217faf6733d52e
Oscar Calle <ocalle374@gmail.com>
2023-07-11 17:23:45 -0400

    Added SauceList.js

---
+++ client/src/components/SauceList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+
+
+
+function SauceList(props) {
+
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>Sauces</h1>
+            {/* HAVE the hot sauces display */}
+            <h1>brand name:{props.name}</h1>
+            <p>Scoville rating:{props.heatRating}</p>
+
+            <button onClick={() => navigate("/")}>Home</button>
+            <button onClick={() => navigate(-1)}>Go backwards</button>
+            <button onClick={() => navigate(1)}>Go forward 1</button>
+
+        </div>
+
+    )
+};
+
+
+
+export default SauceList;
