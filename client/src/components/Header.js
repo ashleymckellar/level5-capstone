8357814a5a0243df8dc69d7c0da7e7e78641192a
Oscar Calle <ocalle374@gmail.com>
2023-07-11 17:42:55 -0400

    Figure why the SauceListContainer,js duplicates

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function Header(){
+    return (
+        <>
+        
+        </>
+    )
+};
+
+export default Header;
+
+//FOR the Route forward and backward button to display on all pages; no need to code into each route
