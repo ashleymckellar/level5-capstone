f44d37c2db1be7351413c673cdfb9c8aaf0d72e2
Oscar Calle <ocalle374@gmail.com>
2023-07-14 18:58:04 -0400

    fixed merges?

--- client/src/components/SauceList.js
+++ client/src/components/SauceList.js
@@ -14,7 +14,9 @@ function SauceList(props){
             <h1>Sauces</h1>
             {/* HAVE the hot sauces display */}
             <h1>brand name:{props.name}</h1>
+            <h4>birth place: {props.origin}</h4>
             <p>Scoville rating:{props.heatRating}</p>
+            <p>Ingredients: {props.ingredients}</p>
 
             <button onClick={() => navigate("/")}>Home</button>
             <button onClick={() => navigate(-1)}>Go backwards</button>
